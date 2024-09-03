ef20ecc667133d183ecd1ffb45db4cdbd27443ff
Carlos Hernandez <hernandez_2002@tamu.edu>
2024-09-02 20:12:48 -0500

    basic pages, adding CRUD

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+  def index
+    @books = ['Book 1', 'Book2', 'Book3']
+  end
+
+  def show 
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  root "main#index"
+
+  get 'books/index'
+  get 'books/new'
+  get 'books/edit'
+  get 'books/delete'
+  get 'books/show'
+
+
+
+  # match 'about', to "main#index", via: :get
+
+  get 'main/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
